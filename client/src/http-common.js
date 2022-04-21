d462d9e11cfe73e5c253e4921ac22335ed7c2946
Eli Gadot <eli.gadot@gmail.com>
2022-04-21 13:14:19 +0300

    changed backend base url for production

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://185.28.152.24:8080/api",
   headers: {
     "Content-type": "application/json"
   }
