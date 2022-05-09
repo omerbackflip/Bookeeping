367779ff7dd4d42cae146fbee73bb0d2a6a9bb2b
Eli Gadot <eli.gadot@gmail.com>
2022-05-09 10:49:18 +0300

    Added env for dev and prod

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.VUE_APP_API_URL,
   // baseURL: "http://185.28.152.24:3000/api",
 
   headers: {
