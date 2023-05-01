6864cce9daecaeb32b482d05eeee66967cc064a7
Eli Gadot <eli.gadot@gmail.com>
2023-05-01 14:35:05 +0300

    switch DB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,6 +2,6 @@
 // with db.config.js file like this:
 
 module.exports = {
-  url: "mongodb://localhost:27017/bookeeping"
-  // url: "mongodb+srv://eli:dg76jqRdOKa0Czlu@cluster0.l0i9tnp.mongodb.net/booking?retryWrites=true&w=majority"
+  // url: "mongodb://localhost:27017/bookeeping"
+  url: "mongodb+srv://eli:dg76jqRdOKa0Czlu@cluster0.l0i9tnp.mongodb.net/booking?retryWrites=true&w=majority"
 };
